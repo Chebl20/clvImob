5ac607e47c7bb364f04a4b16846cf4fc80e29a3b
Chebl <jtchebl@outlook.com>
2024-07-18 20:04:49 -0300

    ajeitando alguns erros e um bugs no index

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import MongoStore from 'connect-mongo';
+
 // import de rota
 
 
@@ -26,3 +27,29 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors({ origin: 'http://localhost:3000/', methods: "GET,POST,PUT,DELETE", credentials: true }));
 app.use("/assets", express.static(path.join(dirname, 'public/assets')));
+
+
+// Configuração do multer para armazenamento de arquivos
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname, 'public/assets'));
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    }
+  });
+  const upload = multer({ storage });
+
+
+
+
+//   rotas
+// fazer rotas aqui
+
+
+// conexao do mongo
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => {
+    app.listen(process.env.PORT, () => console.log(`Server running on port: ${process.env.PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`)); 
